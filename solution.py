72471d99eb3394980f2a0c57ea6ab9e5511f0b40
Ihab Sultan <ihabsultan@gmail.com>
2017-02-01 22:17:41 -0800

    Diagonal Sudoku
    Adding solution for diagonal Sudoku

--- solution.py
+++ solution.py
@@ -144,7 +144,9 @@ boxes = cross(rows, cols)
 row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
-unitlist = row_units + column_units + square_units
+diagonal_units = [[rows[i] + cols[i] for i in range(len(rows))]]
+offdiagonal_units  = [[rows[i] + cols[len(cols)-i-1] for i in range(len(rows))]]
+unitlist = row_units + column_units + square_units + diagonal_units + offdiagonal_units
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
 
